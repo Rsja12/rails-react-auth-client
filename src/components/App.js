fa2c840a5468e6bfd2e336dd361e271a67fcbd94
Rsja12 <asocarras10@gmail.com>
2020-06-23 14:48:05 -0400

    clean frontend

--- src/components/App.js
+++ src/components/App.js
@@ -1,102 +1,11 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-import Login from './Login'
-import SecretsList from './SecretsList'
-
-export class App extends Component {
-
-    state = {
-        currentUser: null,
-        loginForm: {
-            email: '',
-            password: ''
-        },
-        secrets: []
-    }
-
-    componentDidMount() {
-        const token = localStorage.getItem('token')
-        if (token) {
-            fetch('http://localhost:3000/get_current_user', {
-                headers: {
-                    'Authorization': token
-                }
-            })
-            .then(res => res.json())
-            .then(res => console.log(res))
-        }
-
-    }
-
-    handleChange = e => {
-        // grab name and value from e.target
-        // name is the name attr of input field
-        // value is user input
-        const { name, value } = e.target
-
-        // handle input change for both fields 
-        this.setState({
-            loginForm: {
-                ...this.state.loginForm,
-                [name]: value
-            }
-        })
-    }
-
-    handleSubmit = e => {
-        e.preventDefault()
-
-        // submit the info from form to the backend
-        const userInfo = this.state.loginForm
-
-        fetch('http://localhost:3000/login', {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ user: userInfo })
-        })
-        .then(res => res.json())
-        .then(res => {
-            // failure
-            if (res.error) alert('Invalid Credentials')
-
-            // success
-            this.setState({
-                currentUser: res
-            })
-        })
-        // catch js errors
-        .catch(err => console.log(err))
-        
-    }
-
-    getSecrets = () => {
-        fetch('http://localhost:3000/secrets')
-        .then(res => res.json())
-        .then(res => {
-            // failure
-            if (res.error) alert('Not authorized')
-            // success
-            this.setState({
-                secrets: res
-            })
-        })
-        .catch(err => console.log(err))
-    }
-
-    render() {
-        return (
-            <div>
-                <Login
-                    handleChange={ this.handleChange }
-                    handleSubmit={ this.handleSubmit }
-                    email={ this.state.loginForm.email }
-                    password={ this.state.loginForm.password }
-                />
-            </div>
-        )
-    }
+const App = () => {
+    return (
+        <div>
+            App
+        </div>
+    )
 }
 
 export default App
