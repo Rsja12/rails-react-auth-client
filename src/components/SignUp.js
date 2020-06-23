d3fa67984fed1c3918fcdacfc1ca6657b118f6ce
Rsja12 <asocarras10@gmail.com>
2020-06-23 15:50:53 -0400

    create sign up form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,17 +1,57 @@
 import React, { Component } from 'react'
 
 export class SignUp extends Component {
+
+    state = {
+        name: '',
+        email: '',
+        password: ''
+    }
+
+    handleFormChange = e => {
+        const { name, value } = e.target
+        this.setState({
+            [name]: value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        console.log('submit')
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={ this.handleSubmit }>
                 <div className='form-group'>
-                    <input type='text' className='form-control' placeholder='Name' />
+                    <input 
+                        onChange={ this.handleFormChange }
+                        type='text' 
+                        className='form-control' 
+                        placeholder='Name' 
+                        name='name'
+                    />
                 </div>
                 <div className='form-group'>
-                    <input type='email' className='form-control' placeholder='Email' />
+                    <input 
+                        onChange={ this.handleFormChange }
+                        type='email' 
+                        className='form-control' 
+                        placeholder='Email' 
+                        name='email'
+                    />
                 </div>
                 <div className='form-group'>
-                    <input type='password' className='form-control' placeholder='Password' />
+                    <input 
+                        onChange={ this.handleFormChange }
+                        type='password' 
+                        className='form-control' 
+                        placeholder='Password' 
+                        name='password'
+                    />
+                </div>
+                <div className='form-group'>
+                    <input type='submit' className='form-control' value='Submit' />
                 </div>
             </form>
         )
