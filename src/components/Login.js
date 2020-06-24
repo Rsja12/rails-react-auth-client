39a2a20b09e52176912be87110c7c755e8387ae5
Rsja12 <asocarras10@gmail.com>
2020-06-24 15:40:01 -0400

    update Login component to make it controlled

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,53 @@
 import React, { Component } from 'react'
 
 export class Login extends Component {
+
+    state = {
+        userInfo: {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleFormChange = e => {
+        const { name, value } = e.target
+
+        this.setState({
+            userInfo: {
+                ...this.state.userInfo,
+                [name]: value
+            }
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        console.log(this.state.userInfo)
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={ this.handleSubmit }>
                 <div className='form-group'>
-                    <input type='email' className='form-control' placeholder='Email' />
+                    <input 
+                        onChange={ this.handleFormChange }
+                        type='email' 
+                        name='email'
+                        className='form-control' 
+                        placeholder='Email' 
+                    />
                 </div>
                 <div className='form-group'>
-                    <input type='password' className='form-control' placeholder='Password' />
+                    <input 
+                        onChange={ this.handleFormChange }
+                        type='password' 
+                        name='password'
+                        className='form-control' 
+                        placeholder='Password' 
+                    />
+                </div>
+                <div className='form-group'>
+                    <input type='submit' className='form-control' value='Submit' />
                 </div>
             </form>
         )
