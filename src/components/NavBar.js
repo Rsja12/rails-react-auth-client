11b1fc883cda6ecc42d80a3d392ec3bbae201245
Rsja12 <asocarras10@gmail.com>
2020-07-06 13:56:55 -0400

    remove typo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-import AuthButton from './AuthButton'
-
 const NavBar = () => {
     return (
         <nav className='navbar navbar-dark bg-dark'>
